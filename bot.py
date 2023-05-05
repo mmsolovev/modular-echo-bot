9b52b00f785806a39e0eee21e37bdb627029b664
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2023-05-05 21:21:27 +0300

    add: mvp functional (#1)
    Co-authored-by: mmsolovev <mmsolovevmail@gmail.com>

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import load_config, Config
+from handlers import user_handlers, other_handlers
+
+
+# Запуск бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
